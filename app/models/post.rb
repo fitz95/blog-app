bfbeb1d9b2517fe76502a419f6a9b60db5be3e93
fitz95 <fitzgeraldnsong@gmail.com>
2023-05-04 13:30:42 +0100

    add associations and add data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :integeruser
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_save :update_author_posts_counter
+
+  def update_author_posts_counter
+    author.increment!(:postscounter)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  private :update_author_posts_counter
 end
