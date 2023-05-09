7c2369f531247483001fac28bae8710c01730bf0
fitz95 <fitzgeraldnsong@gmail.com>
2023-05-09 11:19:42 +0100

    add placeholder for post and method in post controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,5 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find_by(id: params[:id])
-        # If the user is not found, redirect to the users index page
-        redirect_to users_path if @user.nil?
     end
 end
