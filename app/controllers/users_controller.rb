4c8090c3387ba0276ae367d068f577999d5d5ad5
fitz95 <fitzgeraldnsong@gmail.com>
2023-05-09 11:04:35 +0100

    update user controller and method as well as placeholder views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
     def index
+        @users = User.all
     end
-    
+
     def show
+        @user = User.find_by(id: params[:id])
+        # If the user is not found, redirect to the users index page
+        redirect_to users_path if @user.nil?
     end
 end
