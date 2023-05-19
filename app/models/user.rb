f721477e941725a7ae9b5597faadb0caa87d6dea
fitz95 <fitzgeraldnsong@gmail.com>
2023-05-19 16:00:28 +0100

    add password change feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@ class User < ApplicationRecord
   validates :postscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def most_three_recent_posts
-    posts.order(created_at: :asc).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, coontrollers: { sessions: 'users/sessions'}
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
